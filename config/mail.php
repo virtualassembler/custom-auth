0677f9ce00baedcbf61467285645272112769164
virtualassembler <virtualassembler@gmail.com>
2020-05-21 09:57:34 -0500

    init commit

---
+++ config/mail.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    "driver" => "log",
+    "host" => "smtp.mailtrap.io",
+    "port" => 2525,
+    "from" => array(
+        "address" => "from@example.com",
+        "name" => "Example"
+    ),
+    "username" => "172724ad016441",
+    "password" => "e3bfedc234992f",
+    "sendmail" => "/usr/sbin/sendmail -bs",
+    'stream' => [
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true,
+        ],
+    ]
+];
